5d01c0a88b3c8c4e583309ae4c3ef2a7b97e76a4
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-09-01 22:18:30 +0300

    Register Creation model with admin

--- project/creation/admin.py
+++ project/creation/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Creation
 
 # Register your models here.
+class CreationAdmin(admin.ModelAdmin):
+    fields = ['name', 'cover_photo', 'description', 'file', 'license']
+
+admin.site.register(Creation, CreationAdmin)
