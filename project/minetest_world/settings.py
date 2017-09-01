dfe761cb1580ab00efb1db4abcc8195fff7e6734
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-09-01 22:24:27 +0300

    Fix typo

--- project/minetest_world/settings.py
+++ project/minetest_world/settings.py
@@ -122,5 +122,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Media urls
-+MEDIA_URL = '/media/'
-+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
