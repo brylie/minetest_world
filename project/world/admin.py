2bb7267c6afaf74681fa531b79413fbf1fc00ed4
Brylie Christopher Oxley <brylie@gnumedia.org>
2017-09-01 22:15:00 +0300

    Register World and WorldRevision with admin

--- project/world/admin.py
+++ project/world/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import World, WorldRevision
+
+class WorldAdmin(admin.ModelAdmin):
+    fields = ['name', 'description', 'cover_photo', 'license']
 
 # Register your models here.
+admin.site.register(World, WorldAdmin)
+admin.site.register(WorldRevision)
